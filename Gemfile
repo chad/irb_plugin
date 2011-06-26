5e09fdc07af7a4e1c6cbe0c0916e6a3e6c23f2b7
Chad Fowler <chad.fowler@livingsocial.com>
2011-06-26 08:33:19 -0600

    look for irb_plugin deps instead of as

--- Gemfile
+++ Gemfile
@@ -1,5 +1,3 @@
 source "http://rubygems.org"
-gem 'activesupport'
-gem 'rails', "3.0.7"
 # Specify your gem's dependencies in irb_plugin.gemspec
 gemspec
--- lib/irb_plugin.rb
+++ lib/irb_plugin.rb
@@ -4,7 +4,7 @@ require "irb_plugin/version"
 module IrbPlugin
   def self.load_all_plugins
     dependent_gems = Gem.cache.select do |_, gemspec|
-      gemspec.dependencies.any?{|dependency| dependency.name == "activesupport"}
+      gemspec.dependencies.any?{|dependency| dependency.name == "irb_plugin"}
     end.map do |name, gemspec|
       begin
         require "#{gemspec.name}/irb_plugin"
